867e65205f8a6f9d43d3e222a526c18fa0b4441b
jain-Anuj <jainanuj9415@gmail.com>
2019-08-24 23:57:34 +0530

    completed login.html

--- tech_blog/tech_blog/urls.py
+++ tech_blog/tech_blog/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls'))
+    path('',include('blog.urls')),
+    path('account/login',views.login,name = 'login')
+    path('account/logout',views.logout, name = 'logout',kwargs = {'next_page':'/'})
 ]
