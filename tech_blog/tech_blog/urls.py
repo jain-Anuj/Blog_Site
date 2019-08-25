94813e5a3d79757f1abf9fcbe73fe8eda982b656
jain-Anuj <jainanuj9415@gmail.com>
2019-08-25 14:45:35 +0530

    completed all the html and css part

--- tech_blog/tech_blog/urls.py
+++ tech_blog/tech_blog/urls.py
@@ -21,6 +21,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('account/login',views.login,name = 'login')
-    path('account/logout',views.logout, name = 'logout',kwargs = {'next_page':'/'})
+     path('accounts/login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
